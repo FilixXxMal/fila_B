744665e23ad9aaa0c8b134445ea5aaeb2078694b
FilixXxMal <maldifassi.filippo.maria@iisgalvanimi.edu.it>
2019-11-18 09:52:17 +0000

    punto 4

--- b-end/app.js
+++ b-end/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const path = require('path')
+const path = require('path');
 
 app.use(express.static(__dirname + '/../www/'));
 
@@ -8,6 +8,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/../www/index.html'));
 });
 
+app.get('/api/mante', function (req, res) {
+  res.sendFile(path.join(__dirname+'/mante.json'));
+});
 
 app.listen(3000, function () {
   console.log('in ascolto');
